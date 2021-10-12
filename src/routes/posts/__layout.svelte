a0468cae9ebedf25973f4223f83396fb04e1cb93
Antonio Rossi <info@antoniorossi.net>
2021-10-12 12:04:08 +0100

    routing, slug, markdown implemented

--- src/routes/posts/__layout.svelte
+++ src/routes/posts/__layout.svelte
@@ -3,8 +3,8 @@
 	<aside>
 		<h5>Archive</h5>
 		<ui>
-			<li><a href="">One</a></li>
-			<li><a href="">Two</a></li>
+			<li><a href="/posts">Home</a></li>
+			<li><a href="/posts/about">About</a></li>
 			<li><a href="">Three</a></li>
 			<li><a href="">Four</a></li>
 		</ui>
