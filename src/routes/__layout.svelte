686fdda49dff0d6f5f1cdb4832c4fccf42ffccdc
Antonio Rossi <info@antoniorossi.net>
2021-10-12 08:19:12 +0100

    initial structure

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/header.svelte';
+	import '../tailwind.css';
+</script>
+
+<Header />
+
+<main class="bg-blue-400">
+	<slot />
+</main>
+
+<footer>Copyright 2021</footer>
---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<h1>About Me</h1>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. At maxime molestiae, velit deleniti
+	dolorum accusantium sed adipisci magnam esse praesentium quisquam corporis ad tempore dolorem
+	laborum tenetur explicabo laudantium nisi.
+</p>
