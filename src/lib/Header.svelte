e6fb9f2d0c5d9cf4d71a87aefaff394e49ce7f7f
Antonio Rossi <info@antoniorossi.net>
2021-11-11 21:14:32 +0000

    basic dropdown added

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,6 +3,8 @@
   import { toggleTheme } from '$lib/shared/theme';
   import { Sun, Moon } from '$lib/components/icons/index.js';
   import Logo from './components/Logo.svelte';
+  import ChevronDown from './components/icons/ChevronDown.svelte';
+  import Dropdown from './components/Dropdown.svelte';
 </script>
 
 <header
@@ -16,9 +18,20 @@
           class="flex items-center text-black dark:text-white transition duration-501 ease-in-out"
           href="/">Home</a
         >
+        <button
+          type="button"
+          class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+        >
+          Work
+          <ChevronDown />
+        </button>
+        <Dropdown />
         <a
           class="flex items-center text-black dark:text-white transition duration-501 ease-in-out"
-          href="/about">About</a
+          href="/career">Personal</a
         >
       </div>
     </nav>
--- src/lib/components/icons/index.js
+++ src/lib/components/icons/index.js
@@ -1,4 +1,5 @@
 import Sun from './Sun.svelte';
 import Moon from './Moon.svelte';
+import ChevronDown from './ChevronDown.svelte';
 
-export { Sun, Moon };
+export { Sun, Moon, ChevronDown };
