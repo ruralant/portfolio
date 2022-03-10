316083a0504b37030ec3c3d9f9a696fcbd4ef966
Antonio Rossi <info@antoniorossi.net>
2022-03-10 11:46:31 +0100

    chore: simplify main and blog footer. update packages

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,7 +5,6 @@
 <footer
   class="w-full flex flex-col justify-center items-center bg-spaceWhite dark:bg-spaceGrey transition duration-501 ease-in-out pb-8 pt-20"
 >
-  <p class="text-black dark:text-white text-xs">💻 v0.6.4</p>
   <p class="text-black dark:text-white">
     © {year} Made with ❤️ by Antonio Rossi
   </p>
