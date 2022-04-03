7bc8c7972889b5db7cffdc1aab48634f8ef0761a
Antonio Rossi <info@antoniorossi.net>
2022-04-03 14:13:53 +0100

    fix: home og meta moved out from root

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,6 +36,24 @@
   export let posts;
 </script>
 
+<svelte:head>
+  <title>Antonio Rossi Website</title>
+  <meta
+    name="description"
+    content="Antonio Rossi personal website. Blogging about open source, development and simple leaving."
+  />
+  <meta property="og:title" content="Antonio Rossi Website" />
+  <meta
+    property="og:description"
+    content="Antonio Rossi personal website. Blogging about open source, software development and simple rural leaving"
+  />
+  <meta property="og:url" content="https://www.antoniorossi.net/" />
+  <meta
+    property="og:image"
+    content="https://res.cloudinary.com/antonio-rossi/image/upload/v1633877706/myself.jpg"
+  />
+</svelte:head>
+
 <Hero />
 <Articles {posts} />
 <Contacts />
