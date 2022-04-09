49cd19dc76ae959734fbc25c40c11e741e52b7de
Antonio Rossi <info@antoniorossi.net>
2022-04-09 10:32:48 +0100

    chore: imprve feedly appearence

--- src/routes/rss.xml.js
+++ src/routes/rss.xml.js
@@ -16,12 +16,17 @@ export async function get() {
 
 const xml = (
   posts
-) => `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
+) => `<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:webfeeds="http://webfeeds.org/rss/1.0">
   <channel>
     <title>Antonio Rossi Website</title>
     <id>https://www.antoniorossi.net/rss.xml</id>
     <link type="text/html" href="https://www.antoniorossi.net/" rel="alternate"/>
     <description>Blogging about open source, software development and simple rural leaving</description>
+    <webfeeds:cover image="https://www.antoniorossi.net/favicon.svg" />
+    <webfeeds:icon>https://www.antoniorossi.net/favicon.svg</webfeeds:icon>
+    <webfeeds:logo>https://www.antoniorossi.net/favicon.svg</webfeeds:logo>
+    <webfeeds:accentColor>000000</webfeeds:accentColor>
+    <webfeeds:related layout="card" target="browser"/>
     ${posts
       .map(
         (post) =>
