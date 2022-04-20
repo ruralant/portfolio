d9362dd701dd2b190212bf4124fcff2084291de6
Antonio Rossi <info@antoniorossi.net>
2022-04-20 08:08:58 +0100

    test: testing fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -46,7 +46,17 @@
       }
     }
   </script>
-  <meta name="theme-color" content={localTheme === 'dark' ? '#000' : '#fff'} />
+  <!-- <meta name="theme-color" content={localTheme === 'dark' ? '#000' : '#fff'} /> -->
+  <meta
+    name="theme-color"
+    content="#fff"
+    media="(prefers-color-scheme: light)"
+  />
+  <meta
+    name="theme-color"
+    content="#000"
+    media="(prefers-color-scheme: dark)"
+  />
 </svelte:head>
 
 <div id="core" class={localTheme}>
