df97e87c590642848c4363fecc521a0e44ee1551
Antonio Rossi <info@antoniorossi.net>
2022-05-03 20:33:33 +0100

    fix: remove nexted link in blog list

--- src/lib/components/blog/BlogListItem.svelte
+++ src/lib/components/blog/BlogListItem.svelte
@@ -5,16 +5,14 @@
 
 <li class="font-Poppins md:w-full mt-5">
   <a
-    class="flex w-full p-4 justify-between text-left items-center rounded-md overflow-hidden shadow-md bg-white dark:bg-neutral-900 hover:scale-105"
+    class="flex w-full p-4 justify-between text-left items-center rounded-md overflow-hidden shadow-md bg-white dark:bg-neutral-900 hover:scale-105 transition duration-300 ease-in-out"
     href={`/blog/${post.type}/${post.slug}`}
   >
     <div>
-      <a href={`/blog/${post.type}/${post.slug}`}>
-        <span
-          class="title-text font-Cormorant text-mediumTitle color-white m-0 bg-gradient-271 from-darkFromColor via-lightViaColor1 via-darkViaColor2 to-lightToColor dark:from-darkFromColor dark:via-darkViaColor1 dark:via-darkViaColor2 dark:to-darkToColor"
-          >{post.title}</span
-        >
-      </a>
+      <span
+        class="title-text font-Cormorant text-mediumTitle color-white m-0 bg-gradient-271 from-darkFromColor via-lightViaColor1 via-darkViaColor2 to-lightToColor dark:from-darkFromColor dark:via-darkViaColor1 dark:via-darkViaColor2 dark:to-darkToColor"
+        >{post.title}</span
+      >
       <div class="flex my-4">
         <span
           class="capitalize mr-2 inline-flex rounded-md shadow-sm px-2 py-1 bg-white text-xs text-neutral-600 bg-neutral-200 dark:text-neutral-300 dark:bg-neutral-800"
