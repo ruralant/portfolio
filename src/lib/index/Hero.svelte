c64fc87c713527ec13734087627f19428ec42add
Antonio Rossi <info@antoniorossi.net>
2022-05-09 20:09:15 +0100

    style: css removed from Hero

--- src/lib/index/Hero.svelte
+++ src/lib/index/Hero.svelte
@@ -5,30 +5,35 @@
   import Image from '$lib/components/Image.svelte';
 </script>
 
-<div class="flex justify-center items-center flex-wrap	pt-10 sm:pt-9">
-  <div class="text-container">
-    <div class="title-container">
+<div class="flex justify-center items-center flex-wrap pt-10 sm:pt-9">
+  <div class="text-container flex-2 md:min-w-[660px]">
+    <div
+      class="flex flex-row justify-center align-center md:justify-start md:align-start"
+    >
       <h1 class="flex items-center flex-nowrap">
         <span>
           <span
-            class="title-text font-Cormorant bg-gradient-271 from-darkFromColor via-darkViaColor1 via-lightViaColor2 to-lightToColor dark:from-darkFromColor dark:via-darkViaColor1 dark:via-darkViaColor2 dark:to-darkToColor "
+            class="font-Cormorant text-6xl md:text-8xl bg-gradient-271 from-darkFromColor via-darkViaColor1 via-lightViaColor2 to-lightToColor dark:from-darkFromColor dark:via-darkViaColor1 dark:via-darkViaColor2 dark:to-darkToColor leading-standard m-0"
             >Hi, I&apos;m Antonio</span
           >
           <span class="text-5xl ml-1 lg:ml-5 md:hidden lg:inline">👋</span>
         </span>
       </h1>
     </div>
-    <div class="subtitle font-Poppins text-3xl">
+    <div
+      class="subtitle font-Poppins text-3xl font-light mt-2.5 leading-extraRelaxed"
+    >
       <p class="text-neutral-800 dark:text-white ">
         I'm a full stack developer in Reading, UK.
       </p>
       {' '}
       <p class="text-neutral-800 dark:text-white ">
         I'm interested in{' '}
-        <span class="interactive-text">open source</span>,
-        <span class="interactive-text">energy efficiency</span> of the web,
-        <span class="interactive-text">homesteading</span>{' '}and{' '}
-        <span class="interactive-text">soil regeneration</span>.
+        <span class="md:font-bold w-[200px]">open source</span>,
+        <span class="md:font-bold w-[200px]">energy efficiency</span> of the
+        web,
+        <span class="md:font-bold w-[200px]">homesteading</span>{' '}and{' '}
+        <span class="md:font-bold w-[200px]">soil regeneration</span>.
       </p>
     </div>
   </div>
@@ -43,67 +48,3 @@
     classes={'rounded-full overflow-hidden mt-10 xl:mt-0 lg:ml-20'}
   />
 </div>
-
-<style>
-  .text-container {
-    flex: 2;
-    min-width: 600px;
-  }
-  .title-container {
-    display: flex;
-    flex-direction: row;
-  }
-  .title-text {
-    color: #fff;
-    /* font-weight: 500; */
-    margin: 0;
-    line-height: 1.15;
-    font-size: 6rem;
-  }
-  .subtitle {
-    font-weight: 300;
-    margin-top: 10px;
-    line-height: 1.7;
-  }
-  .subtitle span {
-    width: 200;
-  }
-  .interactive-text {
-    /* font-size: 2.5rem; */
-    font-weight: 700;
-    /* /* font-weight: 500; */
-    /* color: var(--color-text-primary);
-    -webkit-text-fill-color: transparent;
-    -webkit-text-stroke-width: 1.4px;
-    -webkit-text-stroke-color: var(--color-text-primary); */
-  }
-  .interactive-text:hover {
-    -webkit-text-fill-color: var(--color-text-primary);
-  }
-  @media (max-width: 800px) {
-    .title-text {
-      font-size: 4rem;
-    }
-    .subtitle {
-      font-size: 1.5rem;
-    }
-    .interactive-text {
-      font-weight: 500;
-      font-size: 1.5rem;
-      -webkit-text-fill-color: var(--color-text-primary);
-      -webkit-text-stroke-color: transparent;
-    }
-  }
-  @media (max-width: 600px) {
-    .text-container {
-      min-width: unset;
-    }
-  }
-  @media (max-width: 550px) {
-    .title-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  }
-</style>
