63b8033caf6d02aa6631f36f1528f4d537b10aee
Antonio Rossi <info@antoniorossi.net>
2022-10-23 16:21:14 +0100

    feat: improve line height quote. improve accessibility contact icons

--- src/lib/index/Quote.svelte
+++ src/lib/index/Quote.svelte
@@ -1,8 +1,6 @@
 <div class="flex justify-center pt-10 lg:pt-20">
   <div class="flex flex-col items-center bg-grey-100 dark:bg-neutral-900 rounded-lg max-w-2xl p-5">
-    <h2
-      class="text-center font-Cormorant text-xl md:text-2xl text-grey-800 dark:text-white leading-standard italic"
-    >
+    <h2 class="text-center font-Cormorant text-xl md:text-2xl text-grey-800 dark:text-white italic">
       “A quiet secluded life in the countryside, with the possibility of being useful to people to
       whom it is easy to do good, and who are not accustomed to have it done to them; then work
       which one hopes may be of some use; then rest, nature, books, music, love for one's neighbor —
