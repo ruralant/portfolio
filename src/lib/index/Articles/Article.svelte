9d484f5123e5196dc7bb0f6d8e49ea0945633a9b
Antonio Rossi <info@antoniorossi.net>
2022-10-23 15:29:48 +0100

    chore: fix indentation

--- src/lib/index/Articles/Article.svelte
+++ src/lib/index/Articles/Article.svelte
@@ -1,31 +1,31 @@
 <script>
-	export let postData;
-	export let postPath;
-	export let { title, subtitle, tags, icon, date, type, slug } = postData;
+  export let postData;
+  export let postPath;
+  export let { title, subtitle, tags, icon, date, type, slug } = postData;
 </script>
 
 <li
-	class="w-full max-w-sm h-60 rounded-md overflow-hidden bg-white dark:bg-neutral-900 shadow-md p-6"
+  class="w-full max-w-sm h-60 rounded-md overflow-hidden bg-white dark:bg-neutral-900 shadow-md p-6"
 >
-	<a class="h-full flex flex-col justify-between" href={postPath}>
-		<div>
-			<div
-				class="text-neutral-800 dark:text-white bg-gradient-90 from-purple-600 to-red-500 dark:from-purple-500 dark:to-red-400 font-bold text-xl mb-2 "
-			>
-				{title}
-			</div>
-			<p class="text-neutral-800 dark:text-white">
-				{subtitle}
-			</p>
-		</div>
-		<div>
-			{#each tags as tag}
-				<span
-					class="capitalize mr-2 inline-flex rounded-md shadow-sm px-2 py-1 text-xs text-neutral-600 bg-neutral-200 dark:text-neutral-300 dark:bg-neutral-700"
-				>
-					{tag}
-				</span>
-			{/each}
-		</div>
-	</a>
+  <a class="h-full flex flex-col justify-between" href={postPath}>
+    <div>
+      <div
+        class="text-neutral-800 dark:text-white bg-gradient-90 from-purple-600 to-red-500 dark:from-purple-500 dark:to-red-400 font-bold text-xl mb-2 "
+      >
+        {title}
+      </div>
+      <p class="text-neutral-800 dark:text-white">
+        {subtitle}
+      </p>
+    </div>
+    <div>
+      {#each tags as tag}
+        <span
+          class="capitalize mr-2 inline-flex rounded-md shadow-sm px-2 py-1 text-xs text-neutral-600 bg-neutral-200 dark:text-neutral-300 dark:bg-neutral-700"
+        >
+          {tag}
+        </span>
+      {/each}
+    </div>
+  </a>
 </li>
