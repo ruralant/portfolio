cbc4a331eacf99e1e27135e6d808306b8836bb42
Antonio Rossi <info@antoniorossi.net>
2023-02-10 14:28:16 +0000

    fix: playwright tests

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -1,28 +1,30 @@
 import { expect, test } from "@playwright/test";
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto("/");
-// });
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
 
-// test("index page has expected h1", async ({ page }) => {
-//   expect(await page.textContent("h1")).toBe("Hi, I'm Antonio 👋");
-// });
+test.describe("Home page", () => {
+  test("should show the title", async ({ page }) => {
+    await expect(page.getByText("Hi, I'm Antonio 👋")).toBeVisible();
+  });
 
-// test("dropdown menu links to blog", async ({ page }) => {
-//   await page.getByRole("button", { name: "Work" }).click();
-//   const linkToArticles = page.getByRole("menuitem", { name: "Articles" });
-//   expect(linkToArticles).toHaveText("Articles");
-//   await linkToArticles.click();
-//   const latestArticles = page.getByRole("heading", { name: "Latest Articles" });
-//   expect(latestArticles).toBeVisible();
-// });
+  test("dropdown menu links to blog", async ({ page }) => {
+    await page.getByRole("button", { name: "Work" }).click();
+    const linkToArticles = page.getByRole("menuitem", { name: "Articles" });
+    await expect(linkToArticles).toHaveText("Articles");
+    await linkToArticles.click();
+    const latestArticles = page.getByRole("heading", { name: "Latest Articles" });
+    await expect(latestArticles).toBeVisible();
+  });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
+  // test("get started link", async ({ page }) => {
+  //   await page.goto("https://playwright.dev/");
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
+  //   // Click the get started link.
+  //   await page.getByRole("link", { name: "Get started" }).click();
 
-//   // Expects the URL to contain intro.
-//   await expect(page).toHaveURL(/.*intro/);
-// });
+  //   // Expects the URL to contain intro.
+  //   await expect(page).toHaveURL(/.*intro/);
+  // });
+});
