74cc01c749c5e9a4faaf2e830e224f0d0bc3e327
Antonio Rossi <info@antoniorossi.net>
2023-02-17 20:55:28 +0000

    feat: update contacts. move icons to components. update packages

--- src/lib/components/icons/Rss.svelte
+++ src/lib/components/icons/Rss.svelte
@@ -1,30 +1,16 @@
 <svg
+  xmlns="http://www.w3.org/2000/svg"
   width="24"
   height="24"
-  stroke-width="1.5"
   viewBox="0 0 24 24"
-  class="h-6 w-6 stroke-current stroke-grey-800 dark:stroke-white"
   fill="none"
-  xmlns="http://www.w3.org/2000/svg"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="h-6 w-[1.4rem] stroke-current stroke-grey-800 dark:stroke-white"
 >
-  <title id="rss-icon-title">Rss feed</title>
-  <desc id="rss-icon-desc">Rss feed icon</desc>
-  <path
-    d="M12 19C12 14.8 9.2 12 5 12"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M19 19C19 10.6 13.4 5 5 5"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M5 19.01L5.01 18.9989"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
+  <path d="M4 11a9 9 0 0 1 9 9" />
+  <path d="M4 4a16 16 0 0 1 16 16" />
+  <circle cx="5" cy="19" r="1" />
 </svg>
