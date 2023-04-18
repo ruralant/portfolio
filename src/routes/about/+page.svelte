ebeb140f717eff0ad027da37b650b95b8c6790d7
Antonio Rossi <info@antoniorossi.net>
2023-04-18 20:18:15 +0100

    feat: add description, basic style and update package

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,36 @@
+<h1>About me</h1>
+
+<div
+  class="font-Roboto lg:prose-xl text-neutral-900 dark:text-white dark:prose-invert prose-a:text-teal-400 prose-pre:max-w-xs prose-pre:min-w-full prose-pre:whitespace-pre-wrap prose-pre:text-left md:prose-pre:max-w-full prose-pre:bg-neutral-200 dark:prose-pre:bg-neutral-800 prose-pre:text-neutral-700 dark:prose-pre:text-neutral-100 prose-code:bg-neutral-200 dark:prose-code:bg-neutral-800 prose-code:text-neutral-700 dark:prose-code:text-neutral-100 tracking-wide"
+>
+  <p>
+    My name is Antonio and I am a software engineer with a passion for developing green software. I
+    specialize in React, Svelte, Node, AWS, and Typescript, and I have years of experience
+    developing efficient and sustainable software solutions.
+  </p>
+
+  <p>
+    I strongly believe in the importance of creating software that is environmentally responsible,
+    and I am committed to incorporating eco-friendly practices into my work whenever possible. By
+    focusing on reducing energy consumption, minimizing waste, and using sustainable resources, I
+    strive to create software that is efficient, cost-effective and environmentally conscious.
+  </p>
+
+  <p>
+    When I'm not coding, you usually find me trail running in the countryside or climbing. I have a
+    deep appreciation for nature and enjoy spending time in the great outdoors. I enjoy traveling
+    slow, by train, bicycle, or on foot. I did the Camino de Santiago and several other
+    long-distance walks and hikes.
+  </p>
+
+  <p>
+    In my free time, I also grow my own food in an allotment, which allows me to practice
+    sustainable living and connect with the earth.
+  </p>
+
+  <p>
+    Thank you for taking your time to learn a little but more about myself. If you have any
+    questions, if we have passions in common, or uf you just want to say hi, do not hesitate to
+    contact me at hello@antoniorossi.net
+  </p>
+</div>
