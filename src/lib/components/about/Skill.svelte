0852258e6c941d2c9b4b4cb956fdece24b2db71e
Antonio Rossi <info@antoniorossi.net>
2023-07-14 23:01:32 +0100

    fix: skills lines now showing

--- src/lib/components/about/Skill.svelte
+++ src/lib/components/about/Skill.svelte
@@ -6,10 +6,11 @@
   // const percentage = (skill.value / maxValue) * 100;
 </script>
 
-<div>
-  <div class="flex items-center justify-between">
-    <span class="mr-2">{skill.skill}</span>
-    <!-- <div>
+{#if skill && percentage}
+  <div>
+    <div class="flex items-center justify-between">
+      <span class="mr-2">{skill.skill}</span>
+      <!-- <div>
       {#if skill.love}
         <span>❤️</span>
       {/if}
@@ -20,36 +21,36 @@
         <span>🚀</span>
       {/if}
     </div> -->
-  </div>
-  <div class="progress-bar w-full h-4 bg-slate-200 dark:bg-neutral-800">
-    <div class="progress-bar-fill bg-progressBarBlue" style={`width: ${percentage}%`}>
-      <!-- <span>{skill.text}</span> -->
+    </div>
+    <div class="progress-bar w-full h-4 bg-slate-200 dark:bg-neutral-800">
+      <div class="progress-bar-fill bg-progressBarBlue" style={`width: ${percentage}%`}>
+        <!-- <span>{skill.text}</span> -->
+      </div>
     </div>
   </div>
-</div>
-
-<style>
-  .progress-bar {
-    border-radius: 0.25rem;
-    margin-bottom: 1rem;
-  }
-  .progress-bar-fill {
-    height: 100%;
-    /* background-color: rgb(168, 85, 247); */
-    border-radius: 0.25rem;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    /* animation: progress 1s ease-in-out forwards; */
-  }
-  /* 
-  .progress-bar-fill span {
-    padding-right: 0.5rem;
-    color: #fff;
-  } */
-  /* @keyframes progress {
-    0% {
-      width: 0%;
+  <style>
+    .progress-bar {
+      border-radius: 0.25rem;
+      margin-bottom: 1rem;
     }
-  } */
-</style>
+    .progress-bar-fill {
+      height: 100%;
+      /* background-color: rgb(168, 85, 247); */
+      border-radius: 0.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      /* animation: progress 1s ease-in-out forwards; */
+    }
+    /* 
+    .progress-bar-fill span {
+      padding-right: 0.5rem;
+      color: #fff;
+    } */
+    /* @keyframes progress {
+      0% {
+        width: 0%;
+      }
+    } */
+  </style>
+{/if}
