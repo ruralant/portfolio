c584a13c967ab6e4e879754511fd59d458dbd6dd
Antonio Rossi <info@antoniorossi.net>
2023-08-29 21:47:19 +0100

    feat: pagination visually implemented

--- src/lib/components/blog/Pagination.svelte
+++ src/lib/components/blog/Pagination.svelte
@@ -6,6 +6,9 @@
 
 <div>
   {#each Array(pages) as page, i}
-    <button on:click={nextPosts(i * 10, (i + 1) * 10)} class="p-3">{i + 1}</button>
+    <button
+      class="w-10 h-10 mt-10 mx-2 rounded-full shadow-sm bg-white dark:bg-neutral-800 text-neutral-600 dark:text-neutral-300"
+      on:click={nextPosts(i * 10, (i + 1) * 10)}>{i + 1}</button
+    >
   {/each}
 </div>
