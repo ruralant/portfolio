622bcd6134ae08147892e9070912244d23a5bfa0
Antonio Rossi <info@antoniorossi.net>
2023-09-13 09:00:12 +0100

    style: remove hover scale from tag

--- src/lib/components/blog/Tag.svelte
+++ src/lib/components/blog/Tag.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <a
-  class="capitalize mr-2 inline-flex rounded-md shadow-sm px-2 py-1 text-xs text-neutral-600 bg-neutral-200 dark:text-neutral-300 dark:bg-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-700 hover:scale-105"
+  class="capitalize mr-2 inline-flex rounded-md shadow-sm px-2 py-1 text-xs text-neutral-600 bg-neutral-200 dark:text-neutral-300 dark:bg-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-700"
   href={url}
 >
   {tagName}
