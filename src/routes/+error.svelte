be49c83a898a255054722f878dc1456fbd88f6e2
Antonio Rossi <info@antoniorossi.net>
2023-10-03 19:38:20 +0100

    fix: handle error if blog params do not match article

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,6 +12,16 @@
   <title>{title}</title>
 </svelte:head>
 <div class="flex flex-col justify-center items-center">
-  <h1 class="text-neutral-800 dark:text-neutral-100">Something weird happened</h1>
-  <h2 class="text-neutral-800 dark:text-neutral-100">{title}</h2>
+  <div class="text-5xl md:text-4xl mt-10 mb-10">😱</div>
+  <h1 class="text-4xl text-neutral-800 dark:text-neutral-100 mb-3">
+    Wops, Something weird happened
+  </h1>
+  <h2 class="text-3xl text-neutral-800 dark:text-neutral-100 mb-3">
+    If you were looking for a blog post, I made some changes on the structure of the website.
+  </h2>
+  <h2 class="text-xl text-neutral-800 dark:text-neutral-100">
+    You can find all the blog posts <a class="underline font-medium text-teal-500" href="/blog"
+      >here</a
+    >.
+  </h2>
 </div>
