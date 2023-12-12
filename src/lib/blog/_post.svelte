e810b75b345d03b23e75dcbe5b8ea47c092408e6
Antonio Rossi <info@antoniorossi.net>
2023-12-12 19:50:20 +0000

    feat: add button on top articles. update packages

--- src/lib/blog/_post.svelte
+++ src/lib/blog/_post.svelte
@@ -33,9 +33,15 @@
   out:fade={{ duration: 150 }}
   class="prose font-Roboto font-normal tracking-wide text-neutral-900 dark:prose-invert lg:prose-xl prose-a:text-teal-400 prose-code:bg-neutral-200 prose-code:text-neutral-700 prose-pre:min-w-full prose-pre:max-w-xs prose-pre:whitespace-pre-wrap prose-pre:bg-neutral-200 prose-pre:text-left prose-pre:text-neutral-700 dark:text-neutral-100 dark:prose-code:bg-neutral-800 dark:prose-code:text-neutral-100 dark:prose-pre:bg-neutral-800 dark:prose-pre:text-neutral-100 md:prose-pre:max-w-full"
 >
-  <p class="mt-10 text-center uppercase tracking-wider md:mt-0">
-    {category}
-  </p>
+  <div class="mt-10 flex flex-row items-center justify-between md:mt-0">
+    <a class="inline-block font-Poppins !text-white no-underline" href={`/blog`}>
+      <ArrowLeft />
+    </a>
+    <p class=" text-center uppercase tracking-wider">
+      {category}
+    </p>
+    <div class="w-6"></div>
+  </div>
   <h1 class="mb-4 text-center text-4xl font-normal tracking-normal">
     {title}
   </h1>
