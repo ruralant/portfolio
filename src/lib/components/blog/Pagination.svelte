d623cf92b40de95fbf646bb1665d3339b8916a0d
Antonio Rossi <info@antoniorossi.net>
2024-06-02 16:59:43 +0100

    chore: update packages

--- src/lib/components/blog/Pagination.svelte
+++ src/lib/components/blog/Pagination.svelte
@@ -9,7 +9,7 @@
   {#each Array(pages) as page, i}
     <button
       disabled={currentPage === i + 1}
-      class={`mx-2 mt-10 h-10 w-10 rounded-full bg-white text-neutral-600  shadow-sm disabled:bg-neutral-100 dark:text-neutral-300 ${
+      class={`mx-2 mt-10 h-10 w-10 rounded-full bg-white text-neutral-600 shadow-sm disabled:bg-neutral-100 dark:text-neutral-300 ${
         currentPage === i + 1 ? "dark:text-neutral-1000 dark:bg-neutral-700" : "dark:bg-neutral-800"
       }`}
       on:click={nextPosts(i * 10, (i + 1) * 10)}>{i + 1}</button
