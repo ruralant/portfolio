e554a3677e8839e2de201e5dae3d6ba5d909621e
Antonio Rossi <info@antoniorossi.net>
2026-02-19 11:00:11 +0000

    feat: display verison on frontend

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <script>
-  export const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 </script>
 
 <footer
@@ -7,5 +7,8 @@
 >
   <p class="text-neutral-800 dark:text-neutral-100">
     © 2016-{year} Made with ❤️ by Antonio Rossi
+  </p>
+  <p class="mt-1 text-xs text-neutral-400 dark:text-neutral-500">
+    v{__APP_VERSION__}
   </p>
 </footer>
